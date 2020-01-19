e6891074d0af3c17886a1094bfd659e1a3b57497
Diogo Naka <diogoshindhinaka@gmail.com>
2020-01-19 19:07:41 -0300

    Implementation of advanced features - socketIO

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 
 const Dev = require('../models/Dev');
-
 const parseStringAsArray = require('../utils/parseStringAsArray');
+
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
   async index(req, res) {
@@ -14,6 +15,8 @@ module.exports = {
   async store(req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
   
+    // Verificar se o username é existente
+
     let dev = await Dev.findOne({ github_username });
 
     if (!dev) {
@@ -36,6 +39,16 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      // Filtrar as conexões que estão há no máximo 10km de distância
+      // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray,
+      );
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
     
